92ca21966cac8ddf51a1ce218ff9fa29cc1345f4
RakshitDevra <110564848+RakshitDevra@users.noreply.github.com>
2024-10-21 13:11:38 +0530

    Update vite.config.ts(Comment and uncomment)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
 
-module.exports = {
-  //...
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-};
+
+// module.exports = {
+//   //...
+//   resolve: {
+//     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+//   },
+// };
